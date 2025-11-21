80ee62506f6a08cbc095b5d49a13e959fe67f046
yaseumine <yasminmaharani123@gmail.com>
2025-11-21 10:32:01 +0700

    buat clip behavior untuk memotong gradient melengkung

--- lib/pages/latihan_cart.dart
+++ lib/pages/latihan_cart.dart
@@ -7,99 +7,122 @@ class LatihanCart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Latihan Cart")),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Card(
-                color: Colors.lightGreen,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Text(
-                    "Buat card pake warna",
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.amber,
-                ),
-                child: Text(
-                  "Buat container pake BoxDecoration",
-                  style: TextStyle(fontSize: 16.0),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Card(
-                elevation: 8,
-                color: Colors.cyan,
-                child: Text(
-                  "coba elevation pada card",
-                  style: TextStyle(fontSize: 16.0),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.pinkAccent,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Card(
+                  color: Colors.lightGreen,
+                  child: Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text(
+                      "Buat card pake warna",
+                      style: TextStyle(fontSize: 16.0),
                     ),
-                  ],
+                  ),
                 ),
-                child: Text("Buat shadow dengan container"),
-              ),
-              SizedBox(height: 20.0),
-              Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
+
+                SizedBox(height: 20.0),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.amber,
+                  ),
                   child: Text(
-                    "Card dengan border radius",
+                    "Buat container pake BoxDecoration",
                     style: TextStyle(fontSize: 16.0),
                   ),
                 ),
-              ),
-              Card(
-                elevation: 8,
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Tentang Saya",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 10.0),
-                      Text(
-                        "Abdul karim adalah seorang penting di zaman ini karena dia membuat banyak reformasi baik diseluh bidang apapun.",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.grey[700],
-                          height: 1.5,
-                        ),
-                        textAlign: TextAlign.justify,
+                SizedBox(height: 20.0),
+                Card(
+                  elevation: 8,
+                  color: Colors.cyan,
+                  child: Text(
+                    "coba elevation pada card",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.pinkAccent,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
                       ),
                     ],
                   ),
+                  child: Text("Buat shadow dengan container"),
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text(
+                      "Card dengan border radius",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Card(
+                  elevation: 8,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Tentang Saya",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          "Abdul karim adalah seorang penting di zaman ini karena dia membuat banyak reformasi baik diseluh bidang apapun.",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey[700],
+                            height: 1.5,
+                          ),
+                          textAlign: TextAlign.justify,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Card(
+                  elevation: 8,
+                  color: Colors.blueAccent,
+                  clipBehavior: Clip.antiAlias,
+                  //aku buat clip behavior untuk garis melengkung pada card
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.blueAccent, Colors.yellowAccent],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    child: Text("text", style: TextStyle(fontSize: 16.0)),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
