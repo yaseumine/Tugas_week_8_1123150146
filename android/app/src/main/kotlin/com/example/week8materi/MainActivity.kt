63a00804858f08e3418d1dd9a15f0b7402ac7c9d
yaseumine <yasminmaharani123@gmail.com>
2025-11-21 08:28:53 +0700

    membuat main dart

---
+++ android/app/src/main/kotlin/com/example/week8materi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week8materi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "week 8",
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
