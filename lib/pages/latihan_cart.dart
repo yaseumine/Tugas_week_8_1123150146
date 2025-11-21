49377014e4761587f0c9f1f59355eeb097bb04ba
yaseumine <yasminmaharani123@gmail.com>
2025-11-21 09:06:55 +0700

    membuat container and card 2

--- lib/pages/latihan_cart.dart
+++ lib/pages/latihan_cart.dart
@@ -6,20 +6,34 @@ class LatihanCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Latihan Cart"), actions: const []),
+      appBar: AppBar(title: const Text("Latihan Cart")),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          child: const Column(
+          child: Column(
             children: [
-              Card(
+              const Card(
                 color: Colors.lightGreen,
+                child: Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "Buat card pake warna",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20.0),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.amber,
+                ),
                 child: Text(
-                  "Buat card pake warna",
+                  "Buat container pake BoxDecoration",
                   style: TextStyle(fontSize: 16.0),
                 ),
               ),
-              SizedBox(height: 20.0),
             ],
           ),
         ),
